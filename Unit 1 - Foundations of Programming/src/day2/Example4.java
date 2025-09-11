c8e52ac9f08eb815e0ad3fd39f3ac307e756323f
Dark-303 <156324532+Dark-303@users.noreply.github.com>
2025-09-11 09:06:07 -0400

    I forgots to uploads lol

---
+++ Unit 1 - Foundations of Programming/src/day2/Example4.java
@@ -0,0 +1,11 @@
+package day2;
+
+public class Example4 {
+    public static void main(String[] args) {
+        System.out.print("1");
+        System.out.println("2");
+        System.out.print("3");
+        System.out.print("4");
+        System.out.println("5");
+    }
+}
