b679faffbdfe1f792f5a975e714540a14fcd9ec7
Dark-303 <156324532+Dark-303@users.noreply.github.com>
2025-09-25 12:44:49 -0400

    UNIT 2222222

---
+++ Unit 2 - Methods, Using Objects and APIs/src/day1/Main.java
@@ -0,0 +1,20 @@
+package day1;
+
+public class Main {
+    public void main(String[] args) {
+        Dog dog1 = new Dog("Steve", 6);
+        Dog dog2 = new Dog("Coco", 9);
+
+        dog2.bark(12);
+        dog1.eat("Chicken Tacos");
+        dog2.eat("people");
+
+        System.out.println(dog1);
+        System.out.println(dog2);
+
+        dog1 = dog2;
+
+        System.out.println(dog1);
+        System.out.println(dog2);
+    }
+}
