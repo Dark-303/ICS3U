c5e3fd98af7c769d26263ec68ae9df473218fb9d
Dark-303 <156324532+Dark-303@users.noreply.github.com>
2026-01-07 09:24:57 -0500

    scoreboard

--- Unit 6 - Creating Classes/src/day2/Scoreboard.java
+++ Unit 6 - Creating Classes/src/day2/Scoreboard.java
@@ -1,3 +1,32 @@
+package day2;
+
 public class Scoreboard {
-    
+    private String team1;
+    private String team2;
+
+    private int team1Score;
+    private int team2Score;
+    private boolean team1Active;
+
+    public Scoreboard(String team1, String team2) {
+        this.team1 = team1;
+        this.team2 = team2;
+        team1Score = 0;
+        team2Score = 0;
+        team1Active = true;
+    }
+
+    public String toString() {
+        return team1Score + " - " + team2Score + " - " + (team1Active ? team1:team2);
+    }
+
+    public void recordPlay(int points) {
+        if (team1Active && points != 0) team1Score++;
+        else team2Score++;
+        if (points == 0) team1Active = !team1Active;
+    }
+
+    public void getScore() {
+        System.out.println(this.toString());
+    }
 }
